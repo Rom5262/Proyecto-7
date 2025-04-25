86d6c7dc3b2e9c46741287fdd587e94f8cbebd71
Rom5262 <garay2772@gmail.com>
2025-04-25 15:37:16 -0600

    construir histograma

--- app.py
+++ app.py
@@ -15,3 +15,12 @@ if hist_button:
         
            
             st.plotly_chart(fig, use_container_width=True)
+
+
+            import streamlit as st
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
